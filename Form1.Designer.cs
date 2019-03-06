2a6bd99141e7d45f5dba29f7d518222cc5bf53ed
Jonathan Villeda <johnsvill@hotmail.com>
2019-03-05 20:30:05 -0600

    Subiendo proyecto central

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace prGestionAhorros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnRetiro = new System.Windows.Forms.Button();
+            this.btnDeposito = new System.Windows.Forms.Button();
+            this.lstDepositos = new System.Windows.Forms.ListBox();
+            this.lstRetiros = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSaldo = new System.Windows.Forms.TextBox();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAbrir);
+            this.groupBox1.Controls.Add(this.txtMonto);
+            this.groupBox1.Controls.Add(this.txtCliente);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(393, 97);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese los datos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDeposito);
+            this.groupBox2.Controls.Add(this.btnRetiro);
+            this.groupBox2.Location = new System.Drawing.Point(12, 113);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(393, 110);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Transacciones";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnNuevo);
+            this.groupBox3.Controls.Add(this.txtSaldo);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.lstRetiros);
+            this.groupBox3.Controls.Add(this.lstDepositos);
+            this.groupBox3.Location = new System.Drawing.Point(12, 229);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(393, 194);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Historial";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cliente:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Monto:";
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(83, 29);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(180, 20);
+            this.txtCliente.TabIndex = 2;
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(83, 64);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(180, 20);
+            this.txtMonto.TabIndex = 3;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(286, 32);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 4;
+            this.btnAbrir.Text = "Abrir cuenta";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnRetiro
+            // 
+            this.btnRetiro.Location = new System.Drawing.Point(188, 48);
+            this.btnRetiro.Name = "btnRetiro";
+            this.btnRetiro.Size = new System.Drawing.Size(75, 23);
+            this.btnRetiro.TabIndex = 0;
+            this.btnRetiro.Text = "Retiro";
+            this.btnRetiro.UseVisualStyleBackColor = true;
+            this.btnRetiro.Click += new System.EventHandler(this.btnRetiro_Click);
+            // 
+            // btnDeposito
+            // 
+            this.btnDeposito.Location = new System.Drawing.Point(35, 48);
+            this.btnDeposito.Name = "btnDeposito";
+            this.btnDeposito.Size = new System.Drawing.Size(75, 23);
+            this.btnDeposito.TabIndex = 1;
+            this.btnDeposito.Text = "Depósito";
+            this.btnDeposito.UseVisualStyleBackColor = true;
+            this.btnDeposito.Click += new System.EventHandler(this.btnDeposito_Click);
+            // 
+            // lstDepositos
+            // 
+            this.lstDepositos.FormattingEnabled = true;
+            this.lstDepositos.Location = new System.Drawing.Point(6, 45);
+            this.lstDepositos.Name = "lstDepositos";
+            this.lstDepositos.Size = new System.Drawing.Size(120, 134);
+            this.lstDepositos.TabIndex = 0;
+            // 
+            // lstRetiros
+            // 
+            this.lstRetiros.FormattingEnabled = true;
+            this.lstRetiros.Location = new System.Drawing.Point(188, 45);
+            this.lstRetiros.Name = "lstRetiros";
+            this.lstRetiros.Size = new System.Drawing.Size(120, 134);
+            this.lstRetiros.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Depósitos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(185, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Retiros";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(326, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Saldo";
+            // 
+            // txtSaldo
+            // 
+            this.txtSaldo.Location = new System.Drawing.Point(314, 104);
+            this.txtSaldo.Name = "txtSaldo";
+            this.txtSaldo.Size = new System.Drawing.Size(73, 20);
+            this.txtSaldo.TabIndex = 5;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(314, 146);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 6;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 423);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Gestión cuenta de ahorros";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnDeposito;
+        private System.Windows.Forms.Button btnRetiro;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.TextBox txtSaldo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstRetiros;
+        private System.Windows.Forms.ListBox lstDepositos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prGestionAhorros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            desactivarControles();
+        }
+        private double monto;
+        private void activarControles()
+        {
+            txtCliente.Enabled = false; //Cuando se ingrese un valor se desactiva
+            btnAbrir.Enabled = false;
+            txtMonto.Enabled = false;
+            btnDeposito.Enabled = true; //Cuando se desactiven los valores de arriba solo quedan activos estos
+            btnRetiro.Enabled = true;
+        }
+        private void desactivarControles()
+        {
+            txtCliente.Enabled = true; 
+            btnAbrir.Enabled = true;
+            txtMonto.Enabled = true;
+            btnDeposito.Enabled = false; 
+            btnRetiro.Enabled = false;
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            string cliente;
+            //Leer datos
+            cliente = txtCliente.Text;
+            monto = Convert.ToDouble(txtMonto.Text);//Casteo
+
+            if (monto > 0)
+            {
+                activarControles();
+            }
+            else
+            {
+                MessageBox.Show("El monto debe ser mayor o igual que cero", "Gestión de ahorros", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private Double leer(string mensaje)
+        {
+            Double cantidad;
+            cantidad = Convert.ToDouble(Microsoft.VisualBasic.Interaction.InputBox("Ingrese monto a: " + mensaje, "Gestión de ahorros", "0", 100, 0));
+            return cantidad;
+        }
+
+        private void btnDeposito_Click(object sender, EventArgs e)
+        {
+            Double deposito;
+            deposito = leer("Depositar");
+            monto = monto + deposito;
+            lstDepositos.Items.Add(deposito);
+            mostrar();
+        }
+        private void mostrar()
+        {
+            txtSaldo.Text = Convert.ToString(monto);
+        }
+
+        private void btnRetiro_Click(object sender, EventArgs e)
+        {
+            Double retiro;
+            retiro = leer("Retirar");
+            //Evaluamos
+            if(retiro<=monto)
+            {
+                monto = monto - retiro;
+                lstRetiros.Items.Add(retiro);
+                mostrar();
+            }
+            else
+            {
+                MessageBox.Show("La cantidad de retiro es mayor al monto disponible", "Gestión de Ahorros", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtCliente.Clear();
+            txtMonto.Clear();
+            txtSaldo.Clear();
+            lstDepositos.Items.Clear();
+            lstRetiros.Items.Clear();
+            desactivarControles();
+        }
+    }
+}
